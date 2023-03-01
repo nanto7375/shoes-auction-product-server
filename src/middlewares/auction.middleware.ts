d3b8e48e699c3a8f3e865c7ddb76f7fd4fef1cc4
정승우 <seungwoo@jeongseung-us-MacBook-Air.local>
2023-03-01 22:13:06 +0900

    fix: auction 등록 에러처리 수정
    - middleware에서 route로 에러처리 이동
    - 자기 등록 상품에 입찰 불가 처리 추가

--- src/middlewares/auction.middleware.ts
+++ src/middlewares/auction.middleware.ts
@@ -1,30 +1,14 @@
 import { Request, Response, NextFunction } from "express";
-import { badData, badRequest } from "../exceptions/definition.exception";
+import { badData } from "../exceptions/definition.exception";
 import ErrorException from "../exceptions/form.exception";
-import { AuctionService, ProductService } from "../services";
 
-export const checkAuctionPost = async ({ body, headers }: Request, res: Response, next: NextFunction ) => {
+export const checkReqAuctionPost = async ({ body, headers }: Request, res: Response, next: NextFunction ) => {
   const { useruuid: userUuid } = headers;
   const { productUuid, bidPrice } = body;
 
   try {
     if ( !productUuid || !userUuid || !bidPrice ) {
       throw new ErrorException( badData );
-    }
-
-    const [ product, topPriceAuction ] = await Promise.all([ 
-      ProductService.findOneByUuid( productUuid ), 
-      AuctionService.getTopPriceAuction( productUuid ), 
-    ]);
-  
-    if ( !product ) {
-      throw new ErrorException( badData );
-    }
-    if ( bidPrice <= product.price ) {
-      throw new ErrorException( badRequest );
-    }
-    if ( topPriceAuction && ( bidPrice <= topPriceAuction.bidPrice ) ) {
-      throw new ErrorException( badRequest );
     }
     
     next();
--- src/routes/auction.route.ts
+++ src/routes/auction.route.ts
@@ -4,14 +4,33 @@ import ErrorException from '../exceptions/form.exception';
 import { badData, badRequest } from '../exceptions/definition.exception';
 import { resSuccess, responseWrapper } from '../utils/handler';
 import { auctionMw } from '../middlewares';
-import { AuctionService } from '../services';
+import { AuctionService, ProductService } from '../services';
 
 const router = Router();
-const { checkAuctionPost } = auctionMw;
+const { checkReqAuctionPost } = auctionMw;
 
-router.post( '/auction', checkAuctionPost, responseWrapper( async ( req: Request, res: Response ) => {
+router.post( '/auction', checkReqAuctionPost, responseWrapper( async ( req: Request, res: Response ) => {
   const { useruuid: userUuid } = req.headers;
   const { productUuid, bidPrice } = req.body;
+
+  const [ product, topPriceAuction ] = await Promise.all([
+    ProductService.findByUuid( productUuid ), 
+    AuctionService.getTopPriceAuction( productUuid ), 
+  ]);
+
+  if ( !product ) {
+    throw new ErrorException( badData );
+  }
+  if ( userUuid === product.userUuid ) {
+    throw new ErrorException( badRequest );
+  }
+  if ( bidPrice <= product.price ) {
+    throw new ErrorException( badRequest );
+  }
+  if ( topPriceAuction && ( bidPrice <= topPriceAuction.bidPrice ) ) {
+    throw new ErrorException( badRequest );
+  }
+
   const result = await AuctionService.doAuction({ productUuid, userUuid, bidPrice });
 
   resSuccess( res, { result });
