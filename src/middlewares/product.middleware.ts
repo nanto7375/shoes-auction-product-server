5218d563108193b0a4ee61220e415e4c94553c0a
정승우 <seungwoo@jeongseung-us-MacBook-Air.local>
2023-03-02 01:03:34 +0900

    refactor: checkBrand middle - if 조건 대상 형태 수정

--- src/middlewares/product.middleware.ts
+++ src/middlewares/product.middleware.ts
@@ -2,10 +2,18 @@ import { Request, Response, NextFunction } from "express";
 import { badData } from "../exceptions/definition.exception";
 import ErrorException from '../exceptions/form.exception';
 
+const SHOES_BRAND = {
+  nike: 1,
+  adidas: 1,
+  newbalance: 1,
+  handmade: 1,
+};
+
 export const checkBrand = ({ query }: Request, res: Response, next: NextFunction ) => {
   const { brand } = query;
+
   try {
-    if ( brand && ( brand !== 'nike' && brand !== 'adidas' && brand !== 'handmade' && brand !== 'newbalance' ) ) {
+    if ( brand && !SHOES_BRAND[brand as string]) {
       throw new ErrorException( badData, 'wrong brand' );
     }
 
