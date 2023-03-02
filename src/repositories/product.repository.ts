733aa06673f2f50fe7739c2548b3e1bbc9b26e7a
bigboyang <bigboyang@naver.com>
2023-03-02 22:19:13 +0900

    fix: 상품조회 수정

--- src/repositories/product.repository.ts
+++ src/repositories/product.repository.ts
@@ -25,14 +25,15 @@ export const createProduct = async ({ userUuid, name, brand, price, description,
 
 export const getProductAndAutions = async ( productUuid, userUuid ) => {
   const where = { uuid: productUuid };
-  
+  const joinWhere = userUuid ? { userUuid, productUuid } :  { createdAt: null };
+
   // 상품 조회 시, 해당 상품의 경매 정보를 조회
   // userUuid가 있을 경우, 해당 유저가 해당 상품에 좋아요를 눌렀는지 조회
   const product = await Product.findOne({
     where,
     include: [
-      { model: Auction, as: 'auctions', order: [ [ 'bidPrice', 'ASC' ] ], required: false },
-      { model: Like, as: 'likes', attributes: [ 'createdAt' ], where: { userUuid , productUuid }, required: false },
+      { model: Auction, as: 'auctions', attributes: [ 'bidPrice', 'result', 'createdAt' ], order: [ [ 'bidPrice', 'ASC' ] ], required: false },
+      { model: Like, as: 'likes', attributes: [ 'createdAt' ], where: joinWhere , required: false },
     ],
   });
 
